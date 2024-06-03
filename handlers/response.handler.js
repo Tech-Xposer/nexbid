1ed140276b0b798c5476d10975b47c181e403021
Ashutosh Sharma <asharma89505@gmail.com>
2024-06-03 16:53:38 +0530

    update response handler -  added new status codes

--- handlers/response.handler.js
+++ handlers/response.handler.js
@@ -2,25 +2,29 @@ class ApiResponse {
 	static success(res, status = 200, message, data) {
 		res.status(status).json({
 			status: "success",
-			message:message,
+			message: message,
 			data: data,
 		});
 	}
 
-	static error(res, error, status = 500) {
-		res.status(status).json({
-			status: "error",
-			error: error,
+	static created(res, data) {
+		res.status(201).json({
+			status: "success",
+			data: data,
 		});
 	}
 
-	static notFound(res) {
-		res.status(404).json({
-			status: "error",
-			error: "Not found",
+	static noContent(res) {
+		res.status(204).json({
+			status: "success",
 		});
 	}
-
+	static notModified(res) {
+		res.status(304).json({
+			status: "error",
+			error: "Not modified",
+		});
+	}
 	static badRequest(res, error) {
 		res.status(400).json({
 			status: "error",
@@ -41,7 +45,18 @@ class ApiResponse {
 			error: error,
 		});
 	}
-
+	static notFound(res) {
+		res.status(404).json({
+			status: "error",
+			error: "Not found",
+		});
+	}
+	static notAcceptable(res, error) {
+		res.status(406).json({
+			status: "error",
+			error: error,
+		});
+	}
 	static conflict(res, error) {
 		res.status(409).json({
 			status: "error",
@@ -49,8 +64,63 @@ class ApiResponse {
 		});
 	}
 
+	static unprocessableEntity(res, error) {
+		res.status(422).json({
+			status: "error",
+			error: error,
+		});
+	}
+
+	static preconditionFailed(res, error) {
+		res.status(412).json({
+			status: "error",
+			error: error,
+		});
+	}
+
+	static tooManyRequests(res, error) {
+		res.status(429).json({
+			status: "error",
+			error: error,
+		});
+	}
+
+	
+
 	static internalServerError(res, error) {
 		res.status(500).json({
+			status: "error",
+			error: error,
+		});
+	}
+	static error(res, error, status = 500) {
+		res.status(status).json({
+			status: "error",
+			error: error,
+		});
+	}
+	static notImplemented(res, error) {
+		res.status(501).json({
+			status: "error",
+			error: error,
+		});
+	}
+	static badGateway(res, error) {
+		res.status(502).json({
+			status: "error",
+			error: error,
+		});
+	}
+
+	static serviceUnavailable(res, error) {
+		res.status(503).json({
+			status: "error",
+			error: error,
+		});
+	}
+
+	static gatewayTimeout(res, error) {
+		res.status(504).json({
 			status: "error",
 			error: error,
 		});
